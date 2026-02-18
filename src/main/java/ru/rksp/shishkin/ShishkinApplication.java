8be470b47e2e30a6fd932a416c9328241fa57ce8
Anton <annnt9na@yandex.ru>
2026-02-18 15:51:52 +0300

    Created springboot project

---
+++ src/main/java/ru/rksp/shishkin/ShishkinApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.shishkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShishkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShishkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/shishkin/ShishkinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.shishkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShishkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
